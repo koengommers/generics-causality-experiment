aa91e05a83d8b6cd47ae381e12825bc9deb0aba2
Koen Gommers <koen@koengommers.nl>
2020-05-31 23:20:37 +0200

    Replace starting text

--- packages/client/src/scenes/StartScene.js
+++ packages/client/src/scenes/StartScene.js
@@ -1,9 +1,12 @@
 import Phaser from 'phaser';
 
 const INTRO_TEXT = `
-Welcome to Fantasiastan! Everything you see here might look familiar, but it is not. Everything is a bit different here. Pay attention to what you see.
+Welcome to Farland! 
+In this study, we would like you to learn about the country of Farland. Everything is a bit different here, so please pay attention to what you see. You will be able to explore the five main towns in the country and then we will ask you some questions about what you observed during your visit. 
 
-Use arrow keys to control the character. Press an arrow key to start.
+Before we start, please maximize this page on your browser so that it is in full screen and make sure that you can, as much as possible, do the rest of the study in a calm environment without distractions or interruptions. 
+
+During the study, use the arrow keys to control the character. Press an arrow key to start.
 `;
 
 export default class StartScene extends Phaser.Scene {
@@ -23,7 +26,7 @@ export default class StartScene extends Phaser.Scene {
         bottom: 10
       },
       wordWrap: {
-        width: 300
+        width: 400
       }
     });
     text.setOrigin(0.5, 0);
